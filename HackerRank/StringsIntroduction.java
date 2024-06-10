abe53e99bd50c407fdebf302554dcae0d75af5c8
Aakash Tanwar <121627724+aa-ka-sh-tan-war@users.noreply.github.com>
2024-06-10 03:23:50 +0000

    Twelveth program done.

---
+++ HackerRank/StringsIntroduction.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class StringsIntroduction{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int l= A.length()+B.length();
+        System.out.println(l);
+        if(A.charAt(0)>B.charAt(0))
+        {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        System.out.println(A.substring(0,1).toUpperCase()+ A.substring(1)+" "+ B.substring(0,1).toUpperCase()+ B.substring(1));
+        sc.close();
+    }
+}
